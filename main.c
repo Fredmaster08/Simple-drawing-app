74c495e454d71baee00a18687281f0c2591cbd4e
Fredmaster08 <frxdmaster@gmail.com>
2025-10-30 19:10:27 +0100

    first window

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 
 #include <stdio.h>
+#include "raylib.h"
 
 int main() {
 
-    printf("Hello World!\n");
+    int screenwidth = 800;
+    int screenHeight = 450;
+
+    InitWindow(screenwidth, screenHeight, "Fredmaster08 & arina1431");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        DrawText("Jo wsp!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+    CloseWindow();
 
     return 0;
 }
