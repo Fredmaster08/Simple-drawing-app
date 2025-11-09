b3bd852b5ecd305c59e5dd245355db6b86d1d925
arina1431 <arina09@tropmann.com>
2025-11-09 14:46:24 +0100

    created window

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
 
 #include <stdio.h>
+#include <raylib.h>
 
 int main() {
 
-    printf("Restart IS REAL!\n");
+    //printf("Restart IS REAL!\n");
+
+    const int screenW = 1000;
+    const int screenH = 800;
+
+    InitWindow(screenW, screenH, "drawing window");
+
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose()){
+        
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+    
+    
+    CloseWindow();
 
     return 0;
 }
