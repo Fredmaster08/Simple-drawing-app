7660275639261bb90a921cb104bc1caaa7238d13
Fredmaster08 <frxdmaster@gmail.com>
2025-11-13 18:02:06 +0100

    messy code, but moving and erasing and, still no storing

--- draw.c
+++ draw.c
@@ -7,7 +7,12 @@
 void draw(Brush* brushes, int* index, Camera2D* camera) {
     Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), *camera);
     int brushSize = 20;
-    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT) && IsKeyDown(KEY_LEFT_CONTROL)) {
+        Vector2 delta = GetMouseDelta();
+        delta = Vector2Scale(delta, -1.0f/camera->zoom);
+        camera->target = Vector2Add(camera->target, delta);
+    }
+    else if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
         int i = *index;
         brushes[i].position = mousePos;
         brushes[i].color = BLUE;
@@ -16,6 +21,16 @@ void draw(Brush* brushes, int* index, Camera2D* camera) {
             DrawCircle(brushes[i].position.x, brushes[i].position.y, brushSize, brushes[i].color);
         }
     }
+    else if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+        int i = *index;
+        brushes[i].position = mousePos;
+        brushes[i].color = BLACK;
+        (*index)++;
+        for(int i = 0; i <= *index; i++) {
+            DrawCircle(brushes[i].position.x, brushes[i].position.y, brushSize, brushes[i].color);
+        }
+    }
+    
     
     //if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
     //    Vector2 mousePos = GetMousePosition();
@@ -23,6 +38,16 @@ void draw(Brush* brushes, int* index, Camera2D* camera) {
     //}
 }
 
-void erase(){
-
+void erase(Brush* brushes, int* index, Camera2D* camera){
+    Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), *camera);
+    int brushSize = 20;
+    if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+        int i = *index;
+        brushes[i].position = mousePos;
+        brushes[i].color = BLACK;
+        (*index)++;
+        for(int i = 0; i <= *index; i++) {
+            DrawCircle(brushes[i].position.x, brushes[i].position.y, brushSize, brushes[i].color);
+        }
+    }
 }
