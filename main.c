2dd8722160809aa6ff58e38b74960d3ac89af32a
Fredmaster08 <frxdmaster@gmail.com>
2025-11-14 16:08:08 +0100

    custom colors RGB

--- main.c
+++ main.c
@@ -10,7 +10,6 @@ int main() {
     const int screenH = 800;
 
     InitWindow(screenW, screenH, "drawing window");
-
     SetTargetFPS(60);
 
     Brush brushes[10000];
@@ -18,13 +17,29 @@ int main() {
     Camera2D zoom = {0};
     camera.zoom = 1.0f;
 
-    
     int index = 0;
-    int sizeBrush;  
-    
+    int sizeBrush;
+    int r, g, b;
+
     printf("Sizebrush?: ");
     scanf("%d", &sizeBrush);
+    printf("Enter R G B values:\n");
+    
+    printf("R: ");
+    scanf("%d", &r);
 
+    printf("G: ");
+    scanf("%d", &g);
+
+    printf("B: ");
+    scanf("%d", &b);
+
+    r = Clamp(r, 0, 255);
+    g = Clamp(g, 0, 255);
+    b = Clamp(b, 0, 255);
+
+    Color brushColor = {r, g, b, 255};
+ 
     while (!WindowShouldClose()){
         
         BeginDrawing();
@@ -36,7 +51,7 @@ int main() {
         wheel(&camera);
 
         for(int i = 0; i <= index; i++) {
-            DrawCircle(brushes[i].position.x, brushes[i].position.y, sizeBrush, brushes[i].color);
+            DrawCircle(brushes[i].position.x, brushes[i].position.y, sizeBrush, brushColor);
         }
 
         EndMode2D();
